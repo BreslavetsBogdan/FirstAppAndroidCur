f4870ba1c127a52b2e7a4b119f60dcf2fe2aecb7
Bogdan <lbc,fkfyc123>
2017-01-20 19:33:09 +0200

    main

---
+++ app/src/main/java/breslavetsb/oa/org/ua/firstappandroidcur/Main.java
@@ -0,0 +1,15 @@
+package breslavetsb.oa.org.ua.firstappandroidcur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //s
+    }
+}
